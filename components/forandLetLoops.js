44ac45592056634d824289685fe89f2df9fa389e
Alimnfl <mnaufallim234@gmail.com>
2023-05-28 01:48:11 +0700

    Looping for and while

---
+++ components/forandLetLoops.js
@@ -0,0 +1,11 @@
+// 2. For
+for (let i = 0; i < 10; i++) {
+  console.log(`For Loop Number ${i}`);
+}
+
+// 3. While
+let i = 0;
+while (i < 10) {
+  console.log(`While loop Number : ${i}`);
+  i++;
+}
