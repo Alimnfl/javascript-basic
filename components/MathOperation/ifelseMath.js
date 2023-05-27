e8bbaf242bd5ddf9eb542295644956e62980e5ee
Alimnfl <mnaufallim234@gmail.com>
2023-05-28 03:13:15 +0700

    If Else Math (Condition)

---
+++ components/MathOperation/ifelseMath.js
@@ -0,0 +1,9 @@
+const x = 100;
+
+if (x === 10) {
+  console.log('x is 10');
+} else if (x > 10) {
+  console.log('x is greather than 10');
+} else {
+  console.log('x is less than 10');
+}
---
+++ components/MathOperation/ifelseMath1.js
@@ -0,0 +1,6 @@
+const x = 4;
+const y = 10;
+
+if (x > 2 && y > 8) {
+  console.log('x and y higher than comparison numbers');
+}
