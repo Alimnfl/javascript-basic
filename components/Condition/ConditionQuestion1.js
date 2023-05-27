a74259f3f89a8ca4c4c5c3e7bda7ae34bd1744b9
Alimnfl <mnaufallim234@gmail.com>
2023-05-28 03:12:36 +0700

    ConditionQuestion

---
+++ components/Condition/ConditionQuestion1.js
@@ -0,0 +1,16 @@
+const x = 10;
+
+// const color = x > 10 ? 'red' : 'blue';
+const color = 'green';
+
+switch (color) {
+  case 'red':
+    console.log('Color is red');
+    break;
+  case 'blue':
+    console.log('Color is blue');
+    break;
+  default:
+    console.log('color is NOT red or blue');
+    break;
+}
