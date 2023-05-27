1379a3c147c0d6ec86dee08c915bc8e1e7b67000
Alimnfl <mnaufallim234@gmail.com>
2023-05-28 03:13:37 +0700

    Arrow Function Operation Math Simple

---
+++ components/MathOperation/ArrowFunctionOperation.js
@@ -0,0 +1,13 @@
+// 1. First Way
+const addNumbs = (num1, num2) => {
+  console.log(num1 + num2);
+};
+
+addNumbs(100, 200);
+
+// 2. Second Way
+// const addNumbs = (num1, num2) => {
+//     return num1 + num2;
+//   };
+
+//   console.log(addNumbs(1, 2));
---
+++ components/MathOperation/FunctionOperation.js
@@ -0,0 +1,13 @@
+// 1. First Way
+function addNumbs(num1, num2) {
+  console.log(num1 + num2);
+}
+
+addNumbs(5, 4);
+
+// 2. Second Way
+// function addNumbs(num1, num2) {
+//     return num1 + num2;
+//   }
+
+//   console.log(addNumbs(1, 2));
